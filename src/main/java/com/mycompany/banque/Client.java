07b67a94521ffd474bd19b6ea7fc5bde3d594b7d
mohcinelamtanez <lamtanezmohcine95@gmail.com>
2026-06-16 03:02:54 +0100

    first commit

---
+++ src/main/java/com/mycompany/banque/Client.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.banque;
+import java.time.* ; 
+/**
+ *
+ * @author USER
+ */
+public class Client {
+   private int Id ;
+   private String nom ; 
+   private String prenom ; 
+   private LocalDate dateNaissance ; 
+   private String adresseMail ; 
+   private LocalDate dateCreation ; 
+   
+   
+   public int getId() { 
+       return this.Id ; 
+   }
+   
+   public void setId(int Id) { 
+       this.Id = Id ; 
+   }
+   
+   public String getNom(){ 
+       return this.nom ; 
+   }
+   
+   public void setNom(String nom){ 
+       this.nom = nom ; 
+   }
+   
+   public String getPrenom(){ 
+       return this.prenom ; 
+   }
+   
+   public void setPrenom(String prenom){ 
+       this.prenom = prenom ; 
+   }
+   
+   public LocalDate getDateNaissance(){ 
+       return this.dateNaissance; 
+   }
+   
+   public void setDateNaissance(LocalDate dateNaissance){ 
+       this.dateNaissance = dateNaissance ; 
+   }
+   
+   public String getAdresseMail() { 
+       return this.adresseMail ; 
+   }
+   
+   public void setAdresseMail(String adresseMail){ 
+       this.adresseMail = adresseMail ; 
+   }
+   
+   public LocalDate getDateCreation(){
+       return this.dateCreation; 
+   }
+   
+   public void setDateCreation(LocalDate dateCreation ){
+       this.dateCreation = dateCreation  ;
+   }
+}
