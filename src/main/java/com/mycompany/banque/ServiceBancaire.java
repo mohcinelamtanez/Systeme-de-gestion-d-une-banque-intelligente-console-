55b85a106da75948888f84864aa5b9ce0be8c2db
mohcinelamtanez <lamtanezmohcine95@gmail.com>
2026-06-19 02:18:01 +0100

    add handling exceptions and services

--- src/main/java/com/mycompany/banque/ServiceBancaire.java
+++ src/main/java/com/mycompany/banque/ServiceBancaire.java
@@ -10,7 +10,7 @@ import java.util.*;
  * @author USER
  */
 public class ServiceBancaire {
-      
+     // logique pour ajouter un client 
     public void ajouterClient(String nom , String prenom , LocalDate dateNaissance , String adresseMail){ 
         Client newClient = new Client() ; 
         newClient.setNom(nom) ; 
@@ -23,6 +23,7 @@ public class ServiceBancaire {
             System.out.println("Client ajouté avec succès ! ") ; 
         }else System.out.println("ajout échoué !") ; 
     }
+    // logique pour supprimer un client 
     
     public void SupprimerClient(int id) {
         boolean exists = Banque.clients.stream().anyMatch(client -> client.getId() == id) ; 
@@ -34,6 +35,7 @@ public class ServiceBancaire {
             System.out.println("client introuvable ! ") ; 
         }
     }
+    // logique pour rechercher un client par id 
     
     public Client rechercherClient(int id){
         List<Client> clientfiltered = Banque.clients.stream().filter(client -> client.getId() == id).toList() ; 
@@ -45,6 +47,8 @@ public class ServiceBancaire {
             return null ;  
      }
     
+    //logic pour ouvrir un compte 
+    
     public void ouvrirCompte(Client client , Compte c , double solde ){ 
         c.setDateOuverture(LocalDate.now());
         c.setNumeroCompte("12333333"); 
@@ -52,4 +56,53 @@ public class ServiceBancaire {
         c.setSolde(solde);
         
     }
+    
+    // logic pour chercher un compte par nombre 
+    
+      public Compte AccountByNumber(String numeroCompte) {
+          
+      List<Compte> foundedAccount = comptes.stream().
+                                            filter(compte -> compte.getNumeroCompte().equals(numeroCompte)).
+                                            toList() ; 
+
+        return foundedAccount.getFirst(); 
+  
+} 
+      
+     public void effectuerTransaction(Transaction transaction) { 
+         if(peutEffectuerTransaction()){
+           transactions.add(transaction);
+          System.out.println("transaction ajouté avec succès" ) ;    
+            }else System.out.println("echec lors de l'jout de la transaction") ; 
+          } 
+
+     public boolean  peutEffectuerTransaction() { 
+          return plafondTransactionQuotidien > calculerNombreTransactionparJour() ; } 
+  
+     public int calculerNombreTransactionparJour() { 
+      List<Transaction> filteredTransactions =   transactions.stream().filter(transaction -> transaction.getdate() ==  Localedate.now()).toList() ; 
+
+         return filteredTransactions.size() ; 
+       }
+  
+     
+
+        public void effectuerDepot(Client client , Compte compte , double montant) { 
+           if(client.comptes.contains(compte)){
+          c.deposer(montant); 
+         } else {
+         throw new AccountNotFoundException(
+            "Le compte n'appartient pas à ce client"
+        );
+    } 
+}
+
+ 
+    public void retirerArgent(double montant) { this.solde -= montant ; } 
+    
+    public void effectuerRetrait(Client client , Compte c , double montant) { 
+        if(client.comptes.contains(c)){ c.retirerArgent(montant) ; 
+        }else if(montant < c.getsolde()) { 
+            throw new InsufficientBalanceException( "Le compte n'appartient pas à ce client" );
+        }else throw new AccountNotFoundException("compte introuvable") ; }
 }
