39a4cf1dd2998a6b3d785050594d72ab8dc69cf7
mohcinelamtanez <lamtanezmohcine95@gmail.com>
2026-06-23 22:41:22 +0100

    add methods in the service

---
+++ src/main/java/com/mycompany/banque/service/ServiceBancaire.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.banque.service;
+
+import com.mycompany.banque.Client;
+import com.mycompany.banque.Data.DataInitializer;
+import com.mycompany.banque.repository.Implementation.InMemoryClientRepository;
+import com.mycompany.banque.repository.Implementation.InMemoryCompteRepository;
+import com.mycompany.banque.repository.Implementation.InMemoryTransactionRepository;
+import com.mycompany.banque.repository.Interface.ClientRepository;
+import com.mycompany.banque.repository.Interface.Repository;
+
+import java.security.Provider;
+import java.util.List;
+
+/**
+ *
+ * @author USER
+ */
+public class ServiceBancaire {
+
+
+    List<Client> clients = DataInitializer.initClients() ;
+    InMemoryClientRepository clientRepo = new InMemoryClientRepository(clients) ;
+    InMemoryCompteRepository compteRepo = new InMemoryCompteRepository(clients , clientRepo) ;
+    InMemoryTransactionRepository transactionRepo = new InMemoryTransactionRepository(clients) ;
+
+
+
+      public void ajouterClient(Client client) {
+          if (clientRepo.existsById(client.getId())) {
+              System.out.println("client existe déjà");
+          } else
+              clientRepo.save(client);
+      }
+
+
+}
